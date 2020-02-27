e51a4edb6ea8c109c8186053baf18547c9e83eeb
andyhorn <andyjhorn@gmail.com>
2020-02-27 11:58:35 -0600

    added error handling in license scanner for open files

--- src/components/license/RenameButton.vue
+++ src/components/license/RenameButton.vue
@@ -1,5 +1,8 @@
 <template>
-  <b-button @click="fix" variant="success">Fix!</b-button>
+  <div class="d-flex flex-row-reverse align-items-center">
+    <b-button @click="fix" variant="success" class="float-right">Fix!</b-button>
+    <p v-if="error" class="text-danger m-0 p-0 pr-2">{{ error }}</p>
+  </div>
 </template>
 
 <script>
@@ -9,15 +12,27 @@ import log from "../../utils/log";
 export default {
   name: "ConfigFixButton",
   props: ["oldPath", "newPath"],
+  data() {
+    return {
+      error: null
+    }
+  },
   methods: {
     fix() {
       log.verbose("[RenameButton] button click detected");
       log.debug(`[RenameButton.vue] old path: ${this.oldPath}`);
       log.debug(`[RenameButton.vue] new path: ${this.newPath}`);
       log.verbose("[RenameButton] renaming file...");
-      fs.renameSync(this.oldPath, this.newPath);
-      log.verbose("[RenameButton] rename complete!");
-      this.$emit("renameComplete");
+      try {
+        fs.renameSync(this.oldPath, this.newPath);
+        log.verbose("[RenameButton] rename complete!");
+        this.$emit("renameComplete");
+      } catch (e) {
+        log.error("RenameButton.vue] unable to rename directory, error:");
+        if (e.code == "EBUSY") {
+          this.error = "Error: Close open files, then try again.";
+        }
+      }
     }
   }
 };
